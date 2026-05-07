4e7d695caced2720700570f94634c632f4b82841
FaabNatiC <fabian.wurl@googlemail.com>
2026-05-07 13:13:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // TuS Esingen Handball App - Service Worker
 // Version bei Updates erhöhen, damit Browser den Cache aktualisiert
-const CACHE_VERSION = 'v23';
+const CACHE_VERSION = 'v24';
 const CACHE_NAME = `tus-esingen-${CACHE_VERSION}`;
 
 // Dateien, die beim Installieren in den Cache wandern (App-Shell)
